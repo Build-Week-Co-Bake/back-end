6b8adf4e1dfe15b60f91ef4b3f6ca88968db0183
cladams0203 <cladams0203@gmail.com>
2020-06-22 12:18:51 -0400

    temp data objects

---
+++ dataTemp.js
@@ -0,0 +1,19 @@
+user = {
+  id: 1,
+  email: "some email",
+  password: "pass",
+  name: "user name",
+  profilePic: "some url",
+};
+
+issue = {
+  id: 1,
+  userId: "foreign to user",
+  title: "issue title",
+  city: "location of issue/ toLowerCase",
+  hoa: "if available hoa name",
+  description: "description of issue",
+  photo: "photo of issue",
+  upvotes: 0,
+  createdAt: "new Date().toLocaleDateString()",
+};
